3866273f53ed665ff645aea8c198ca2fbf2d8d00
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2023-10-27 12:35:08 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import VueSelectList from 've-selectlist'
+import "ve-selectlist/dist/VueSelectList.css"
+</script>
+
+<template>
+  <div>
+    <VueSelectList  :data="categories" optionText="category_name" title="Select Category" @update="onCategriesUpdate" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    VueSelectList
+  },
+  data(){
+  return {
+    categories: [
+            {
+                id: 3,
+                company_id: 1,
+                category_name: "jjjjj"
+            },
+            {
+                id: 4,
+                company_id: 1,
+                category_name: "new main"
+            }
+        ],
+        category_id: "",
+  }
+},
+methods: {
+  onCategriesUpdate(opject) {
+    this.category_id = opject.id
+  }
+}
+}
+</script>
